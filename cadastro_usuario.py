148b6ef9b9309096c6b50aea19a7533ec427d0e6
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2022-03-26 16:45:17 -0300

    commit inicial
    este software tem por finalidade cadastrar usuários em um data base

---
+++ cadastro_usuario.py
@@ -0,0 +1,47 @@
+import json
+
+arquivo = open("db.csv", "a")
+
+arquivo.write("\nprimeiro software")
+
+
+arquivo.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from tkinter import *
+# 
+# root = Tk()
+# root.title("Meu software")
+# root.geometry("800x800")
+# root.resizable(False, False)
+# 
+# button = Button(root, text="Clique", bg="#000", pady=40).grid()
+# root.mainloop()
+
+
+
+# from tkinter import *
+# 
+# root = Tk()
+# root.title("Meu software")
+# root.geometry("800x800")
+# root.resizable(False, False)
+# 
+# button = Button(root, text="Clique", bg="#000", pady=40).grid()
+# root.mainloop()
